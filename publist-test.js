7cbe3ac4dedc0dba947f95e32c4d267be43b55ba
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-15 18:16:36 +0800

    Updated server/client test.

--- publist-test.js
+++ publist-test.js
@@ -3,4 +3,6 @@ const publist = require( "./publist.js" );
 
 assert.ok( publist( { "name": "simple" } ) );
 
+assert.equal( typeof publist( { "name": "simple" } ), "object", "should return object" );
+
 console.log( "ok" );
