8dc9d933cf185cb372448e13fe8f4e5c4f6ce395
johnlenon <johnlenonmaghanoy@gmail.com>
2017-08-13 16:20:13 +0800

    Updated test.

--- publist-test.js
+++ publist-test.js
@@ -1,8 +1,8 @@
 const assert = require( "assert" );
 const publist = require( "./publist.js" );
 
-assert.ok( publist( { "name": "simple" } ) );
+assert.ok( publist( { "name": "simple" } ), "should not throw error" );
 
-assert.equal( typeof publist( { "name": "simple" } ) == "object", true, "should return true" );
+assert.equal( typeof publist( { "name": "simple" } ) == "object", true, "should be of object data type" );
 
 console.log( "ok" );
