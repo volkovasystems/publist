79a3b1310e426f49efd7672b947ec4291255ba2b
Vinse Vinalon <vinsevinalon@gmail.com>
2017-08-29 12:52:25 +0800

    Update client/server unit test.

--- publist-test.js
+++
@@ -1,8 +0,0 @@
-const assert = require( "assert" );
-const publist = require( "./publist.js" );
-
-assert.ok( publist( { "name": "simple" } ), "should not throw error" );
-
-assert.equal( typeof publist( { "name": "simple" } ) == "object", true, "should be of object data type" );
-
-console.log( "ok" );
